0563c69cb3c18f36b0562f250529dab82eb01791
kansei4649 <kobayashikansei@gmail.com>
2020-08-03 15:41:05 +0900

    m

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
+    if @message.save!
       respond_to do |format|
         format.json
       end
